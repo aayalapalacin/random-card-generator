8e793c10592bfaa4c3d4ec601b27f444a7468c56
avokeys <alexander.ayala.palacin@gmail.com>
2022-02-10 02:16:58 +0000

    created random card generator with html, javascript, and css

--- src/app.js
+++ src/app.js
@@ -5,7 +5,40 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let randomGenerator = () => {
+  let symbol = ["♦", "♥", "♠", "♣"];
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let randomSymbol = symbol[Math.floor(Math.random() * symbol.length)];
+  let randomNumber = number[Math.floor(Math.random() * number.length)];
+
+  document.body.querySelector(".suit1").append(randomSymbol);
+  document.body.querySelector(".number").append(randomNumber);
+  document.body.querySelector(".suit2").append(randomSymbol);
+
+  randomSymbol === "♦" || randomSymbol === "♥"
+    ? document.body
+        .querySelectorAll(".suitColor")
+        .forEach(symbolColor => (symbolColor.style.color = "red"))
+    : document.body
+        .querySelectorAll(".suitColor")
+        .forEach(symbolColor => (symbolColor.style.color = "black"));
+};
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  randomGenerator();
 };
